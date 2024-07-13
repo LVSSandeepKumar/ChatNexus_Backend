412f4ef9d4f866dafc24513cc97694b6e6ace655
LVSSandeepKumar <lvssandeepkumar12@gmail.com>
2024-07-13 10:09:29 +0530

    JWT token verification & Auth Middleware

--- routes/AuthRoute.js
+++ routes/AuthRoute.js
@@ -1,9 +1,11 @@
 import {Router} from "express";
-import { Login, Signup } from "../controllers/AuthController.js";
+import { getUserInfo, Login, Signup } from "../controllers/AuthController.js";
+import { verifyToken } from "../middlewares/AuthMiddleware.js";
 
 const authRoutes = Router();
 
 authRoutes.post("/signup", Signup);
 authRoutes.post("/login", Login);
+authRoutes.get("/user-info",verifyToken, getUserInfo);
 
 export default authRoutes;
